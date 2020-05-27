2be1abe881232a8bd53d5b6a1bfcadb1dd5beb16
HurairaMalik <48237216+HurairaMalik@users.noreply.github.com>
2020-05-28 01:26:09 +0500

    Add files via upload

---
+++ MVCCRUD/Controllers/FoodController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCCRUD.Models;
+
+namespace MVCCRUD.Controllers
+{
+    public class FoodController : Controller
+    {
+       
+        public ActionResult AddFood()
+        {
+            Food obj = new Food();
+            return View("AddFood",obj);
+        }
+        [HttpPost]
+        public ActionResult AddFood(Food obj)
+        {
+            MvcCrudEntities contex = new MvcCrudEntities();
+            contex.customers.Add(new customer() { id=obj.id,name = obj.name, category=obj.category,price=obj.price });
+            contex.SaveChanges();
+           
+            return View("AddFood", obj);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ShowRecords()
+        {
+            MvcCrudEntities contex = new MvcCrudEntities();
+            var showRecords=contex.customers.ToList();
+            return View("ShowRecords",showRecords);
+        }
+
+        public ActionResult EditRecord(int cid)
+        {
+           MvcCrudEntities contex = new MvcCrudEntities();
+           var editRecords = (from item in contex.customers
+                              where item.id == cid
+                              select item).First();
+           return View("EditRecord", editRecords);
+        }
+
+        [HttpPost]
+        public ActionResult EditRecord(customer obj)
+        {
+            MvcCrudEntities contex = new MvcCrudEntities();
+            var editRecords = (from item in contex.customers
+                               where item.id == obj.id
+                               select item).First();
+            editRecords.name = obj.name;
+            editRecords.category = obj.category;
+            editRecords.price = obj.price;
+            contex.SaveChanges();
+
+            var Records = contex.customers.ToList();
+            return View("ShowRecords", Records);
+        }
+
+
+
+
+
+
+
+	}
+}
---
+++ MVCCRUD/Models/Food.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;  
+
+namespace MVCCRUD.Models
+{
+    public class Food
+    {
+        [Required(ErrorMessage = "Please Provide id")]
+       
+         public int id { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide name")]
+        public string name { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please Provide category")]   
+        public string category { get; set; }
+        [Required(ErrorMessage = "Please Provide Last Name")]
+        public int price { get; set; }
+
+
+    }
+}
